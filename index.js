a112b998e295701711b8377e45012b39ee4c0ce5
DHARMENDRA SINGH PATEL <dharmendra.patel26@gmail.com>
2018-07-12 15:25:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const process = require('./process');
 
 var paths = [
-    // '/Users/dharmendra/Documents/repos/project1/package.json',
-    // '/Users/dharmendra/Documents/repos/project2/add-money/package.json'
+    // '/Users/YOUR_NAME/Documents/repos/project1/package.json',
+    // '/Users/YOUR_NAME/Documents/repos/project2/package.json'
     // update your absolute path here like above
 ];
 
